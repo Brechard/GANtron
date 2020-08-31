182d78cbb403d914722967e265fce2882cb1c735
Rodrigo Brechard <rodrigobrechard@gmail.com>
2020-08-31 17:55:52 +0200

    increased size of linear model, introduced learning rate scheduler

--- classifier.py
+++ classifier.py
@@ -94,19 +94,21 @@ class MelLoaderCollate:
 
 
 class Classifier(pl.LightningModule):
-    def __init__(self, n_mel_channels, n_frames, n_emotions, criterion, lr, linear_model, model_size, intended_labels):
+    def __init__(self, n_mel_channels, n_frames, n_emotions, criterion, lr, linear_model, model_size, intended_labels,
+                 epochs):
         super().__init__()
         self.n_mel_channels = n_mel_channels
         self.n_frames = n_frames
         self.criterion = criterion
         self.lr = lr
         self.linear_model = linear_model
+        self.epochs = epochs
         self.val_log = "Validation loss - " + ("Intended" if intended_labels else "Calculated")
         if linear_model:
             self.model = torch.nn.Sequential(
                 module(n_mel_channels * n_frames, model_size),
-                module(model_size, 32),
-                torch.nn.Linear(32, n_emotions),
+                module(model_size, model_size),
+                torch.nn.Linear(model_size, n_emotions),
                 torch.nn.Softmax()
             )
         else:
@@ -117,7 +119,6 @@ class Classifier(pl.LightningModule):
                 torch.nn.Flatten(),
                 # Divide by 2^3 because of max pool
                 torch.nn.Linear(int(n_emotions * (n_mel_channels / 2 ** 3) * (n_frames / 2 ** 3)), n_emotions),
-                torch.nn.Dropout(0.1),
                 torch.nn.Softmax()
             )
 
@@ -139,7 +140,9 @@ class Classifier(pl.LightningModule):
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
-        return optimizer
+        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, self.epochs,
+                                                                  eta_min=1e-8, last_epoch=-1)
+        return [optimizer], [lr_scheduler]
 
     def training_step(self, batch, batch_idx):
         x, smallest_length, y = batch
@@ -255,11 +258,12 @@ def train(vesus_path, use_intended_labels, epochs, learning_rate, batch_size, n_
 
     hparams = HParams()
     model = Classifier(hparams.n_mel_channels, n_frames, n_emotions=5, criterion=criterion, lr=learning_rate,
-                       linear_model=linear_model, model_size=model_size, intended_labels=use_intended_labels)
+                       linear_model=linear_model, model_size=model_size, intended_labels=use_intended_labels,
+                       epochs=epochs)
     wandb_logger = WandbLogger(project='Classifier', name=name, log_model=True)
     wandb_logger.log_hyperparams(args)
     trainer = pl.Trainer(max_epochs=epochs, gpus=1, logger=wandb_logger, precision=precision,
-                         early_stop_callback=EarlyStopping('val_loss', patience=10))
+                         early_stop_callback=EarlyStopping('val_loss', patience=20))
     trainer.fit(model, train_loader, val_loader)
 
 
@@ -277,23 +281,23 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--vesus_path', type=str, required=True, help='Path to audio files')
-    parser.add_argument('--use_intended_labels', type=str2bool, default=False,
+    parser.add_argument('--use_intended_labels', type=str2bool, default=True,
                         help='Use intended emotions instead of voted')
     parser.add_argument('--linear_model', type=str2bool, default=False, help='Use linear model or convolutional')
     parser.add_argument('--epochs', type=int, default=300, help='Number of epochs to train')
-    parser.add_argument('--batch_size', type=int, default=256,
+    parser.add_argument('--batch_size', type=int, default=64,
                         help='Batch size, recommended to use a small one even if it is smaller.')
     parser.add_argument('--lr', type=float, default=1e-3, help='Learning rate')
     parser.add_argument('--n_frames', type=int, default=40, help='Number of frames to use for classification')
     parser.add_argument('--precision', type=int, default=16, help='Precision 32/16 bits')
-    parser.add_argument('--model_size', type=int, default=256, help='Model size')
+    parser.add_argument('--model_size', type=int, default=512, help='Model size')
     parser.add_argument('--mel_offset', type=int, default=20, help='Mel offset when loading the frames')
 
     args = parser.parse_args()
     name = f'{args.batch_size}bs-{args.n_frames}nFrames-{args.lr}LR' \
-           f'-{args.model_size}{"linear" if args.linear_model else "conv"}'\
+           f'-{args.model_size}{"linear" if args.linear_model else "conv"}' \
            f'{"-intendedLabels" if args.use_intended_labels else ""}'
-        # wandb.init(project="Classifier", config=args, name=name)
+    # wandb.init(project="Classifier", config=args, name=name)
 
     train(args.vesus_path, args.use_intended_labels, args.epochs, args.lr, args.batch_size, args.n_frames, name,
           args.precision, args.mel_offset, args.linear_model, args.model_size)
